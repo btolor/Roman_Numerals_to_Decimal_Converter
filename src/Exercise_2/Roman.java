94ade5c751335b15eadba50e8e92c06e290848dd
B <brandontolor@yahoo.com>
2018-10-10 22:59:21 -0600

    Createad the main method and the Roman class

---
+++ src/Exercise_2/Roman.java
@@ -0,0 +1,12 @@
+package Exercise_2;
+
+public class Roman {
+
+    //Variables..
+    String uInputNumeral;
+    String testNumeral;
+    int decimalNumber;
+
+
+    //Methods...
+}
---
+++ src/Exercise_2/Roman_Numerals_to_Decimal_Converter.java
@@ -0,0 +1,15 @@
+package Exercise_2;
+
+import java.util.Scanner;
+
+public class Roman_Numerals_to_Decimal_Converter {
+
+    public static void main(String[] args){
+        //Objects...
+        Roman romanCoverter = new Roman();
+        Scanner scan = new Scanner(System.in);
+        //Variables...
+        String uInput = "";
+    }
+
+}
